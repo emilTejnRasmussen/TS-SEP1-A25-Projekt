1eb4f6da4dba5ad0a0f99b181c5643d27d4cf74d
Emil Tejn Rasmussen <emil@rasie.dk>
2025-12-01 13:38:40 +0100

    Add buttons to mainView

--- src/kloeverly/presentation/controllers/MainViewController.java
+++ src/kloeverly/presentation/controllers/MainViewController.java
@@ -1,5 +1,26 @@
 package kloeverly.presentation.controllers;
 
+import kloeverly.presentation.core.ViewManager;
+
 public class MainViewController
 {
+    public void handleViewResidents()
+    {
+
+    }
+
+    public void handleViewGreenTasks()
+    {
+
+    }
+
+    public void handleViewCommonTasks()
+    {
+
+    }
+
+    public void handleViewExchangeTasks()
+    {
+
+    }
 }
