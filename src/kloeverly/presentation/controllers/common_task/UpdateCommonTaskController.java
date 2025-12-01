ef9706a07da4b5c93cc82c4146e8803cb5253de5
Emil Tejn Rasmussen <emil@rasie.dk>
2025-12-01 15:20:36 +0100

    Create UpdateCommonTask View

---
+++ src/kloeverly/presentation/controllers/common_task/UpdateCommonTaskController.java
@@ -0,0 +1,23 @@
+package kloeverly.presentation.controllers.common_task;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class UpdateCommonTaskController
+{
+    public Label mainTitleLbl;
+    public Label idLbl;
+    public TextField titleTextField;
+    public TextField valueTextField;
+    public TextArea descriptionTextField;
+
+    public void handleSave()
+    {
+    }
+
+    public void handleCancel()
+    {
+    }
+}
