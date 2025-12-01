aa7c228fac43baac33c557560ea5be16de2a7ca2
Emil Tejn Rasmussen <emil@rasie.dk>
2025-12-01 14:53:57 +0100

    Create ViewSingleCommonTask View and controller

---
+++ src/kloeverly/presentation/controllers/common_task/ViewSingleCommonTaskController.java
@@ -0,0 +1,21 @@
+package kloeverly.presentation.controllers.common_task;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class ViewSingleCommonTaskController
+{
+    public Label mainTitleLbl;
+    public Label idLbl;
+    public Label titleLbl;
+    public Label valueLbl;
+    public Label descriptionLbl;
+
+    public void handleUpdate()
+    {
+    }
+
+    public void handleGoBack()
+    {
+    }
+}
