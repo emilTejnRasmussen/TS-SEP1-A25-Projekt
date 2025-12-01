c70ecb415077fe3664610b5e9086736fe6926f4b
Emil Tejn Rasmussen <emil@rasie.dk>
2025-12-01 14:28:54 +0100

    Add feature Add Common task

--- src/kloeverly/presentation/controllers/common_task/AddCommonTaskController.java
+++ src/kloeverly/presentation/controllers/common_task/AddCommonTaskController.java
@@ -1,16 +1,20 @@
 package kloeverly.presentation.controllers.common_task;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import kloeverly.domain.CommonTask;
+import kloeverly.domain.Task;
 import kloeverly.persistence.DataManager;
+import kloeverly.presentation.core.InitializableController;
+import kloeverly.presentation.core.ViewManager;
+import kloeverly.presentation.core.Views;
 
-public class AddCommonTaskController
+public class AddCommonTaskController implements InitializableController
 {
     @FXML
-    private Label titelErrorLbl;
+    private Label titleErrorLbl;
     @FXML
     private TextField titleTextField;
     @FXML
@@ -24,11 +28,53 @@ public class AddCommonTaskController
 
     private DataManager dataManager;
 
-    public void handleAdd(ActionEvent event)
+    @Override
+    public void init(DataManager dataManager)
     {
+        this.dataManager = dataManager;
     }
 
-    public void handleCancel(ActionEvent event)
+    public void handleAdd()
     {
+        if (!validateInput()) return;
+
+        String name = titleTextField.getText().trim();
+        int value = Integer.parseInt(valueTextField.getText().trim());
+        String description = descriptionTextArea.getText().trim();
+        Task task = new CommonTask(name, description, value);
+        dataManager.addTask(task);
+    }
+
+    public void handleCancel()
+    {
+        ViewManager.showView(Views.COMMON_TASKS);
+    }
+
+    private boolean validateInput()
+    {
+        boolean allInputIsValid = true;
+        if (titleTextField.getText().isEmpty()){
+            titleErrorLbl.setText("Titel må ikke være tom");
+            allInputIsValid = false;
+        }
+        if (valueTextField.getText().isEmpty()){
+            valueErrorLbl.setText("Værdi må ikke være tom");
+            allInputIsValid = false;
+        } else {
+            try
+            {
+                Integer.parseInt(valueTextField.getText().trim());
+            } catch (NumberFormatException e)
+            {
+                valueErrorLbl.setText("Værdi skal være et tal");
+                allInputIsValid = false;
+            }
+        }
+        if (descriptionTextArea.getText().isEmpty()){
+            descriptionErrorLbl.setText("Beskrivelse må ikke være tom");
+            allInputIsValid = false;
+        }
+
+        return allInputIsValid;
     }
 }
