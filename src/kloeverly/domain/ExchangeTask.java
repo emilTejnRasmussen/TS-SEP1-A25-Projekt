7faa46f5f33c0d2cf8ac74407fabb72c53656256
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-12-02 18:49:36 +0100

    Overload af completed()

---
+++ src/kloeverly/domain/ExchangeTask.java
@@ -0,0 +1,48 @@
+package kloeverly.domain;
+
+import java.io.Serializable;
+
+public class ExchangeTask extends Task implements Serializable
+{
+  private final Resident provider;
+  private int amount;
+
+  public ExchangeTask(String name, String description, int value, int amount,
+      Resident provider)
+  {
+    super(name, description, value);
+    this.provider = provider;
+    this.amount = amount;
+  }
+
+  public Resident getProvider()
+  {
+    return provider;
+  }
+
+  public int getAmount()
+  {
+    return amount;
+  }
+
+  @Override
+  public void completed(){
+    throw new UnsupportedOperationException("Use completed(Resident resident) instead.");
+  }
+
+  public void completed(Resident resident)
+  {
+    Resident recipient = resident; // recipient gemt for at det er tydeligt hvem, der er modtager og udbyder
+    if (recipient.getPoints() < getValue())
+    {
+      throw new IllegalStateException(
+          "Recipient doesn't have enough points to complete.");
+    }
+    else
+    {
+      provider.addPoints(getValue());
+      recipient.addPoints(-getValue());
+      amount--;
+    }
+  }
+}
