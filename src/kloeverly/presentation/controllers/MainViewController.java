e775009b15196a81dacd9a962e840005d6a6d117
Emil Tejn Rasmussen <emil@rasie.dk>
2025-12-03 16:14:26 +0100

    Add active to active menuButton

--- src/kloeverly/presentation/controllers/MainViewController.java
+++ src/kloeverly/presentation/controllers/MainViewController.java
@@ -1,11 +1,30 @@
 package kloeverly.presentation.controllers;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import kloeverly.presentation.core.ViewManager;
 import kloeverly.presentation.core.Views;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainViewController
 {
+    @FXML
+    private Button residentBtn;
+    @FXML
+    private Button greenBtn;
+    @FXML
+    private Button commonBtn;
+    @FXML
+    private Button exchangeBtn;
+
+    private List<Button> menuButtons;
+
+    public void initialize() {
+        menuButtons = List.of(residentBtn, greenBtn, commonBtn, exchangeBtn);
+    }
+
     public void handleViewResidents()
     {
 
@@ -19,6 +38,8 @@ public class MainViewController
     public void handleViewCommonTasks()
     {
         ViewManager.showView(Views.COMMON_TASKS);
+        removeAllActive();
+        commonBtn.getStyleClass().add("active");
     }
 
     public void handleViewExchangeTasks()
@@ -29,10 +50,16 @@ public class MainViewController
     public void handleShowHomeView()
     {
         ViewManager.showView(Views.HOME);
+        removeAllActive();
     }
 
     public void handleExit()
     {
         Platform.exit();
+    }
+
+    private void removeAllActive()
+    {
+        menuButtons.forEach(b -> b.getStyleClass().remove("active"));
     }
 }
