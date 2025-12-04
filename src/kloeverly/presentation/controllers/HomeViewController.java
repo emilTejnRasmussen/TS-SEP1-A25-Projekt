0dd7e7702eded8e07f74d526ca93a09036a7d6f2
Emil Tejn Rasmussen <emil@rasie.dk>
2025-12-04 09:14:53 +0100

    Add feature resetAllPoints to homeView

--- src/kloeverly/presentation/controllers/HomeViewController.java
+++ src/kloeverly/presentation/controllers/HomeViewController.java
@@ -2,11 +2,15 @@ package kloeverly.presentation.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import kloeverly.persistence.DataManager;
 import kloeverly.presentation.core.InitializableController;
 import kloeverly.presentation.core.ViewManager;
+
+import java.util.Optional;
 
 public class HomeViewController implements InitializableController
 {
@@ -34,7 +38,15 @@ public class HomeViewController implements InitializableController
 
     public void handleResetResidentPoints()
     {
-        System.out.println("Not implemented");
+        Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+        confirm.setTitle("Bekræft nulstilling");
+        confirm.setHeaderText("Nulstil individuelle point");
+        confirm.setContentText("Er du sikker på, at du vil nulstille alle beboers individuelle point?");
+
+        Optional<ButtonType> result = confirm.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK){
+            dataManager.resetPointsForAllResidents();
+        }
     }
 
     public void handleShowExternalScreen()
