578673127d280fc0d97981e8e1c239d21a61dea8
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-12-09 23:21:33 +0100

    Bytteopgaver views + controllere. Oprettelse af flash til bekræftelse. Implementeret i bytteopgaver, men klar til implementering i de andre typer opgaver.

--- src/kloeverly/presentation/controllers/exchange_task/ViewAllExchangeTasksController.java
+++ src/kloeverly/presentation/controllers/exchange_task/ViewAllExchangeTasksController.java
@@ -164,10 +164,4 @@ public class ViewAllExchangeTasksController
   {
     return dataManager.getAllExchangeTasks();
   }
-
-  private void resetTable()
-  {
-    handleSearch();
-    allExchangeTaskTable.getSelectionModel().clearSelection();
-  }
 }
