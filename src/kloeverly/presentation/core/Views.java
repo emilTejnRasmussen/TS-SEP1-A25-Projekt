b6c4e7d950c9f22325e5bbf9073cf7c2f70e2181
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-12-13 09:41:07 +0100

    View for RegisterExchangeTask

--- src/kloeverly/presentation/core/Views.java
+++ src/kloeverly/presentation/core/Views.java
@@ -7,32 +7,32 @@ public enum Views
     EXTERNAL("ExternalScreenView"),
 
     //Common Tasks
-    ADD_COMMON_TASK("common_task/AddCommonTask"), UPDATE_COMMON_TASK(
-        "common_task/UpdateCommonTask"), REGISTER_COMMON_TASK(
-        "common_task/RegisterCommonTask"), COMMON_TASK(
-        "common_task/ViewSingleCommonTask"), COMMON_TASKS(
-        "common_task/ViewAllCommonTasks"),
+    ADD_COMMON_TASK("common_task/AddCommonTask"),
+    UPDATE_COMMON_TASK("common_task/UpdateCommonTask"),
+    REGISTER_COMMON_TASK("common_task/RegisterCommonTask"),
+    COMMON_TASK("common_task/ViewSingleCommonTask"),
+    COMMON_TASKS("common_task/ViewAllCommonTasks"),
 
     // Green Tasks
-
     GREEN_TASKS("green_task/ViewAllGreenTasks"),
     ADD_GREEN_TASK("green_task/AddGreenTask"),
     VIEW_SINGLE_GREEN_TASK("green_task/ViewGreenTask"),
     UPDATE_GREEN_TASK("green_task/UpdateGreenTask"),
     REGISTER_GREEN_TASK("green_task/RegisterGreenTask"),
 
-    // Residents
-
-    RESIDENTS("resident/ViewAllResidents"),
-    ADD_RESIDENT("resident/AddResident"),
-    VIEW_SINGLE_RESIDENT("resident/ViewResident"),
-    UPDATE_RESIDENT("resident/UpdateResident"),
-
     // Exchange Tasks
     EXCHANGE_TASKS("exchange_task/ViewAllExchangeTasks"),
     ADD_EXCHANGE_TASK("exchange_task/AddExchangeTask"),
     VIEW_DETAILED_EXCHANGE_TASK("exchange_task/ViewDetailedExchangeTask"),
-    UPDATE_EXCHANGE_TASK("exchange_task/UpdateExchangeTask");
+    UPDATE_EXCHANGE_TASK("exchange_task/UpdateExchangeTask"),
+    REGISTER_EXCHANGE_TASK("exchange_task/RegisterExchangeTask"),
+
+  // Residents
+    RESIDENTS("resident/ViewAllResidents"),
+    ADD_RESIDENT("resident/AddResident"),
+    VIEW_SINGLE_RESIDENT("resident/ViewResident"),
+    UPDATE_RESIDENT("resident/UpdateResident");
+
 
     private final String view;
 
