3581d0ce1dbd10cc0f4342fa4daa14a184d243bd
Emil Tejn Rasmussen <emil@rasie.dk>
2025-12-13 11:02:21 +0100

    Change register commontask to implement amount

--- src/kloeverly/presentation/controllers/common_task/RegisterCommonTaskController.java
+++ src/kloeverly/presentation/controllers/common_task/RegisterCommonTaskController.java
@@ -3,23 +3,20 @@ package kloeverly.presentation.controllers.common_task;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
+import kloeverly.domain.CommonTask;
 import kloeverly.domain.Resident;
-import kloeverly.domain.Task;
 import kloeverly.persistence.DataManager;
 import kloeverly.presentation.core.AcceptsStringArgument;
 import kloeverly.presentation.core.InitializableController;
 import kloeverly.presentation.core.ViewManager;
 import kloeverly.presentation.core.Views;
-import kloeverly.utility.UtilityMethods;
 
 public class RegisterCommonTaskController implements InitializableController, AcceptsStringArgument
 {
     @FXML
-    private Spinner<Integer> amountSpinner;
-    @FXML
     private Label amountErrorLbl;
+    @FXML
+    private Label amountLbl;
     @FXML
     private Label registerErrorLbl;
     @FXML
@@ -33,32 +30,44 @@ public class RegisterCommonTaskController implements InitializableController, Ac
 
     private DataManager dataManager;
     private int id;
-    private Task selectedTask;
+    private CommonTask selectedTask;
 
     @Override
     public void init(DataManager dataManager)
     {
         this.dataManager = dataManager;
         loadResidents();
-        UtilityMethods.createAmountSpinner(amountSpinner, 1);
     }
 
     public void handleRegister()
     {
         Resident byResident = residentComboBox.getSelectionModel().getSelectedItem();
-        if (byResident == null)
-        {
-            registerErrorLbl.setText("Vælg venligst en beboer.");
-            return;
-        }
 
-        for (int i = 0; i < amountSpinner.getValue(); i++)
-        {
-            dataManager.completeTask(this.id, byResident);
-        }
+        if (!allInputIsValid(byResident)) return;
+
+        selectedTask.setAmount(selectedTask.getAmount() - 1);
+        dataManager.updateTask(selectedTask);
+        dataManager.completeTask(this.id, byResident);
 
         ViewManager.updateExternalView();
         ViewManager.showView(Views.COMMON_TASKS, null, selectedTask.formatTaskCompleted(byResident));
+    }
+
+    private boolean allInputIsValid(Resident byResident)
+    {
+        boolean allValid = true;
+        if (byResident == null)
+        {
+            registerErrorLbl.setText("Vælg venligst en beboer.");
+            allValid = false;
+        }
+
+        if (selectedTask.getAmount() < 1) {
+            amountErrorLbl.setText("No more spots available");
+            allValid = false;
+        }
+
+        return allValid;
     }
 
     public void handleCancel()
@@ -75,10 +84,11 @@ public class RegisterCommonTaskController implements InitializableController, Ac
     public void setArgument(String argument)
     {
         this.id = Integer.parseInt(argument);
-        this.selectedTask = dataManager.getTaskById(id);
+        this.selectedTask = (CommonTask) dataManager.getTaskById(id);
 
-        this.nameLbl.setText(this.selectedTask.getName());
-        this.valueLbl.setText(this.selectedTask.getValue() + "");
-        this.descriptionLbl.setText(this.selectedTask.getDescription());
+        nameLbl.setText(selectedTask.getName());
+        valueLbl.setText(selectedTask.getValue() + "");
+        amountLbl.setText(selectedTask.getAmount() + "");
+        descriptionLbl.setText(selectedTask.getDescription());
     }
 }
